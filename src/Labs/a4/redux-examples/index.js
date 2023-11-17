10efae620f5b39b053adc8b57d765ff13d130b26
ethan-feldman <ethanfeldman7@gmail.com>
2023-11-17 11:44:50 -0500

    fixes

--- src/Labs/a4/redux-examples/index.js
+++ src/Labs/a4/redux-examples/index.js
@@ -7,7 +7,7 @@ import TodoList from "./todos/TodoList";
 const ReduxExamples = () => {
   return(
     <div>
-      <h2>Redux Examples</h2>
+      <h2>Redux Examplesssss</h2>
       <HelloRedux/>
       <CounterRedux/>
       <TodoList/>
