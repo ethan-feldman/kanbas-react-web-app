e7b9bf77b29e782bccf24bd89664d82a34726ed6
ethan-feldman <ethanfeldman7@gmail.com>
2023-11-17 11:21:21 -0500

    fixed

--- src/Labs/a4/redux-examples/index.js
+++ src/Labs/a4/redux-examples/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import HelloRedux from "./HelloRedux";
+import CounteRedux from "./CounterRedux";
+import TodoList from "./todos/TodoList"
+
 
 const ReduxExamples = () => {
   return(
     <div>
       <h2>Redux Examples</h2>
+      <HelloRedux/>
+      <CounteRedux/>
+      <TodoList/>
     </div>
   );
 };
--- src/Labs/a4/redux-examples/todos/TodoItem.js
+++ src/Labs/a4/redux-examples/todos/TodoItem.js
@@ -5,6 +5,7 @@ import React from "react";
 function TodoItem({
     todo,
   }) {
+    const dispatch = useDispatch();
     return (
       <li key={todo.id} className="list-group-item">
         <button onClick={() => dispatch(deleteTodo(todo.id))}> Delete </button>
